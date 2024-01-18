91614cf0c9fd09b3db1b93f5563f0430c9152fb3
j.r. <h6141362@gmail.com>
2024-01-18 12:22:25 +0200

    i-c

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
-export default function Item() {
+
+
+export default function Item({item}) {
+
+  const navigate = useNavigate();
+
+
+
+
+
+  const navToItemInfo=(e)=>{
+    navigate("")
+  }
+
+
+
   return (
-    <div>
-      
-      Item
-        
-    </div>
+    <>
+      <div className='item' onClick={navToItemInfo}>
+          <img src={item.img}
+           width="100%"></img>
+          <h2>{item.detail}</h2>
+          <h2>{item.location}</h2>
+          <h2>{item.updateDate}</h2>
+      </div>
+    </>
   )
 }
