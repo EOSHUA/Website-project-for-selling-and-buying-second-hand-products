2de5ee2ccaf06195c62c02b009bea02f7f5d1521
kingYose <149393380+kingYose@users.noreply.github.com>
2024-01-28 13:06:39 +0200

    y

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.png'
+import './App.css';
+import Layout from './layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
